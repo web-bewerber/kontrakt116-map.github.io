e56ee5878a37145312fee39d83cbfdacf313bae8
Andrei Osipov <93529335+web-bewerber@users.noreply.github.com>
2025-06-25 14:59:30 +0300

    Create videoData.js

---
+++ videoData.js
@@ -0,0 +1,23 @@
+const videoData = [
+  {
+    title: "Удар «Герани» по Краматорску",
+    link: "https://lostarmour.info/news/udar-geran-kramatorsk-zvezda-14355",
+    date: "2025-06-24",
+    lat: 48.7020936,
+    lon: 37.6308133
+  },
+  {
+    title: "Уничтожение ББМ «Козак» ВСУ",
+    link: "https://lostarmour.info/news/fpv-kozak-icpbtrubikon-14347",
+    date: "2025-06-24",
+    lat: 48.325865,
+    lon: 37.320716
+  },
+  {
+    title: "Штурм позиций ВСУ в районе Ивано-Дарьевки",
+    link: "https://lostarmour.info/news/shturm-poz-ivano-daryevka-omsbr-123-14343",
+    date: "2025-06-24",
+    lat: 48.827085,
+    lon: 38.185488
+  }
+];
